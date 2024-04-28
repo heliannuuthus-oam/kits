8435349c8002d50f94aa6a4a1537324a41a2e583
tangbo <tangbo@lixiang.com>
2024-04-28 17:54:00 +0800

    trigger ci

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,21 @@ export default defineConfig(async ({ command, mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 	return {
 		plugins: [react(), eslint(), pages()],
-
+		build: {
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes("node_modules")) {
+							return id
+								.toString()
+								.split("node_modules/")[1]
+								.split("/")[0]
+								.toString();
+						}
+					},
+				},
+			},
+		},
 		// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 		//
 		// 1. prevent vite from obscuring rust errors
